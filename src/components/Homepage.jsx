3c1d16ad485a0aa74bd4a396c0cc63a3178e1de3
yuossef151 <yuossefgaafar07@gmail.com>
2026-04-01 11:38:31 +0200

    Initial commit

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -445,13 +445,15 @@ export default function Homepage() {
   return (
     <>
       <div className="flex flex-col md:flex-row justify-center items-center py-4 md:py-7 bg-amber-300">
-        <div className="flex flex-col items-center self-start ms-10 md:self-end md:mb-25 mb-4">
+        <div className="flex flex-col items-center self-start lg:self-start lg:mt-10  ms-10 md:self-end md:mb-25 mb-4">
           <div
-            className={`w-12 h-12 sm:w-16 sm:h-20 md:w-20 md:h-20 bg-black ${
-              turn === "white" ? "" : "border-8 border-red-500"
+            className={`w-12 h-12 sm:w-16 sm:h-20 md:w-20 md:h-20 bg-[#949494] ${
+              turn === "white" ? "border-8 border-red-500" : ""
             }`}
-          ></div>
-          <p className="pt-2 text-xl sm:text-2xl md:text-2xl">Black</p>
+          >
+            <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 20 20"><path fill="white" d="M7.725 2.146c-1.016.756-1.289 1.953-1.239 2.59c.064.779.222 1.793.222 1.793s-.313.17-.313.854c.109 1.717.683.976.801 1.729c.284 1.814.933 1.491.933 2.481c0 1.649-.68 2.42-2.803 3.334C3.196 15.845 1 17 1 19v1h18v-1c0-2-2.197-3.155-4.328-4.072c-2.123-.914-2.801-1.684-2.801-3.334c0-.99.647-.667.932-2.481c.119-.753.692-.012.803-1.729c0-.684-.314-.854-.314-.854s.158-1.014.221-1.793c.065-.817-.398-2.561-2.3-3.096c-.333-.34-.558-.881.466-1.424c-2.24-.105-2.761 1.067-3.954 1.929"></path></svg>
+          </div>
+          <p className="pt-2 text-xl sm:text-2xl md:text-2xl text-white">White</p>
         </div>
 
         <div className="flex flex-col items-center">
@@ -569,14 +571,16 @@ export default function Homepage() {
 
         </div>
 
-        <div className="flex flex-col items-center self-end me-10 md:self-start md:mt-25 mt-4">
+        <div className="flex flex-col items-center lg:self-end lg:mb-10 self-end me-10 md:self-start md:mt-25 mt-4">
           <div
-            className={`w-12 h-12 sm:w-16 sm:h-20 md:w-20 md:h-20 bg-white ${
-              turn === "white" ? "border-8 border-red-500" : ""
+            className={`w-12 h-12 sm:w-16 sm:h-20 md:w-20 md:h-20 bg-[#949494] ${
+              turn === "white" ? "" : "border-8 border-red-500"
             }`}
-          ></div>
-          <p className="pt-2 text-xl sm:text-2xl md:text-2xl text-white">
-            White
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill="black" d="M7.725 2.146c-1.016.756-1.289 1.953-1.239 2.59c.064.779.222 1.793.222 1.793s-.313.17-.313.854c.109 1.717.683.976.801 1.729c.284 1.814.933 1.491.933 2.481c0 1.649-.68 2.42-2.803 3.334C3.196 15.845 1 17 1 19v1h18v-1c0-2-2.197-3.155-4.328-4.072c-2.123-.914-2.801-1.684-2.801-3.334c0-.99.647-.667.932-2.481c.119-.753.692-.012.803-1.729c0-.684-.314-.854-.314-.854s.158-1.014.221-1.793c.065-.817-.398-2.561-2.3-3.096c-.333-.34-.558-.881.466-1.424c-2.24-.105-2.761 1.067-3.954 1.929"></path></svg>
+          </div>
+          <p className="pt-2 text-xl sm:text-2xl md:text-2xl text-black">
+            Black
           </p>
         </div>
           <button
