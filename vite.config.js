6b1a3af5cd922a4480a3816ffd8ab5adc9cab9ca
yuossef151 <yuossefgaafar07@gmail.com>
2026-08-21 23:19:45 +0300

    Fix base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
 export default defineConfig({
-   base: "/Chess/",
+  base: "/", 
   plugins: [
     react(),
     tailwindcss(),
-],
-
+  ],
 })
